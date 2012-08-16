7b7b04b944b3ca2790143bad705de76e50d1d543
Stephen Birarda <commit@birarda.com>
2012-08-15 20:09:43 -0600

    remove default for public path

--- resources/nginx_proxy_config.rb
+++ resources/nginx_proxy_config.rb
@@ -11,6 +11,6 @@ attribute :listen, :kind_of => [Array], :default => [80]
 attribute :server_name, :kind_of => [String, Array], :default => node['fqdn']
 attribute :client_max_body_size, :kind_of => String, :default => "4G"
 attribute :keepalive_timeout, :kind_of => Integer, :default => 5
-attribute :public_path, :kind_of => String, :default => "#{node[:nginx][:sites_dir]}/#{application_name}/current"
+attribute :public_path, :kind_of => String
 attribute :access_log_format, :kind_of => String,  :default => "default"
 attribute :proxy_port, :kind_of => Integer, :default => 8000
