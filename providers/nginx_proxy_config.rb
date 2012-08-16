2bf8d1da28a891f4b56337f38e6fbb2a367e60f0
Stephen Birarda <commit@birarda.com>
2012-08-15 20:01:53 -0600

    fix syntax error in string

--- providers/nginx_proxy_config.rb
+++ providers/nginx_proxy_config.rb
@@ -23,7 +23,7 @@ action :before_deploy do
   if new_resource.ssl_enabled
     # if this is an ssl site then grab the certificate and certificate key from the data bag
     # and create the appropriate files so that nginx can use them
-    file "#{node[:nginx][:cert_dir]}/#{new_resource.application_name]}.pem" do
+    file "#{node[:nginx][:cert_dir]}/#{new_resource.application_name}.pem" do
       owner new_resource.owner
       group new_resource.group
       mode "0600"
